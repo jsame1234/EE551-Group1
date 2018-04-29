21f0034799a7cc5f8ccad4e2981abd20439ac239
bjimenez12 <bjimenez@stevens.edu>
2018-04-29 15:28:50 -0400

    Added terminal output

--- runBoth.sh
+++ runBoth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Starting emotion recognition"
+echo "Starting emotion recognition..."
 python emotion_recognition.py poc &
+echo "Starting eye detection..."
 python eyeDetect.py 1 &
